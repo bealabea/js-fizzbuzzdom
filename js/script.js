3663984478c91a42bf1652a9c13f355129fbe73c
bealabea <beatricemarin89@yahoo.it>
2021-12-10 15:25:27 +0100

    Add CSS (MILESTONE3)

--- js/script.js
+++ js/script.js
@@ -17,26 +17,28 @@ for (let i = 1; i <=100; i++) {
 //MILESTONE 2:
 
 // creo una costante per il mio container html
-const containerHtml = document.querySelector('.containerBoxes');
+const containerHtml = document.querySelector('.container-boxes');
 // ripropongo il ciclo for ma questa volta per stampare nel DOM
 for (let i = 1; i <=100; i++) {
     /*ae i miei numeri sono multipli sia di 3 che di 5,
     attraverso l'aggiunta di un elemento Html stampa FizzBuzz*/
     if (i % 3 === 0 && i % 5 === 0) {  
-        containerHtml.innerHTML += `<div class="box">FizzBuzz</div>`;
+        containerHtml.innerHTML += `<div class="box box-fizzbuzz">FizzBuzz</div>`;
     /*altrimenti se i miei numeri sono multipli solo di 3,
     attraverso l'aggiunta di un elemento Html stampa Fizz*/
     } else if (i % 3 === 0) {  
-        containerHtml.innerHTML += `<div class="box">Fizz</div>`;
+        containerHtml.innerHTML += `<div class="box box-fizz">Fizz</div>`;
     /*altrimenti se i miei numeri sono multipli solo di 5,
     attraverso l'aggiunta di un elemento Html stampa Buzz*/
     } else if (i % 5 === 0) {  
-        containerHtml.innerHTML += `<div class="box">Buzz</div>`;
+        containerHtml.innerHTML += `<div class="box box-buzz">Buzz</div>`;
     //altrimenti stampa i numeri in successione
     } else {
         containerHtml.innerHTML += `<div class="box">${i}</div>`; 
     }
 }
 
+
+// MILESTONE 3: aggiungo css e attribuisco le classi nel codice js
 
 
