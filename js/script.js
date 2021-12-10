cec6cc9ccef2670a2ed4a82d62577e61da17c20e
bealabea <beatricemarin89@yahoo.it>
2021-12-10 14:50:37 +0100

    print the MILESTONE 1 procedure in console

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 // creo un ciclo for per stampare in console log i numeri dall'1 al 100
+for (let i = 1; i <=100; i++) {
+    /*console.log(i)*/
 
 //MILESTONE 1:
-//Se i miei numeri sono multipli sia di 3 che di 5 stampa in console log FizzBuzz
-//altrimenti se sono multipli solo di 3 stampa in console log Fizz
-//altrimenti se sono multipli solo di 5 stampa in console log Buzz
-//altrimenti stampa in console log i numeri in successione
+    if (i % 3 === 0 && i % 5 === 0) {  //Se i miei numeri sono multipli sia di 3 che di 5 stampa in console log FizzBuzz
+        console.log('FizzBuzz')
+    } else if (i % 3 === 0) {  //altrimenti se sono multipli solo di 3 stampa in console log Fizz
+        console.log('Fizz')
+    } else if (i % 5 === 0) {  //altrimenti se sono multipli solo di 5 stampa in console log Buzz
+        console.log('Buzz')
+    } else {
+        console.log(i)  //altrimenti stampa in console log i numeri in successione
+    }
+}
+
+
+
+
+
