6143e60218814e281645fe88cdd2c075d90028a9
bealabea <beatricemarin89@yahoo.it>
2021-12-10 15:11:27 +0100

    Prints the numbers in the DOM based on the required characteristics (MILESTONE2)

--- js/script.js
+++ js/script.js
@@ -4,31 +4,39 @@ for (let i = 1; i <=100; i++) {
 
 //MILESTONE 1:
     if (i % 3 === 0 && i % 5 === 0) {  //Se i miei numeri sono multipli sia di 3 che di 5 stampa in console log FizzBuzz
-        console.log('FizzBuzz')
+        console.log('FizzBuzz');
     } else if (i % 3 === 0) {  //altrimenti se sono multipli solo di 3 stampa in console log Fizz
-        console.log('Fizz')
+        console.log('Fizz');
     } else if (i % 5 === 0) {  //altrimenti se sono multipli solo di 5 stampa in console log Buzz
-        console.log('Buzz')
+        console.log('Buzz');
     } else {
-        console.log(i)  //altrimenti stampa in console log i numeri in successione
+        console.log(i);  //altrimenti stampa in console log i numeri in successione
     }
 }
 
 //MILESTONE 2:
 
 // creo una costante per il mio container html
-
+const containerHtml = document.querySelector('.containerBoxes');
 // ripropongo il ciclo for ma questa volta per stampare nel DOM
-
-/*Se i miei numeri sono multipli sia di 3 che di 5,
-attraverso l'aggiunta di un elemento Html stampa FizzBuzz*/
-
-/*Se i miei numeri sono multipli solo di 3,
-attraverso l'aggiunta di un elemento Html stampa Fizz*/
-
-/*Se i miei numeri sono multipli solo di 5,
-attraverso l'aggiunta di un elemento Html stampa Buzz*/
-
+for (let i = 1; i <=100; i++) {
+    /*ae i miei numeri sono multipli sia di 3 che di 5,
+    attraverso l'aggiunta di un elemento Html stampa FizzBuzz*/
+    if (i % 3 === 0 && i % 5 === 0) {  
+        containerHtml.innerHTML += `<div class="box">FizzBuzz</div>`;
+    /*altrimenti se i miei numeri sono multipli solo di 3,
+    attraverso l'aggiunta di un elemento Html stampa Fizz*/
+    } else if (i % 3 === 0) {  
+        containerHtml.innerHTML += `<div class="box">Fizz</div>`;
+    /*altrimenti se i miei numeri sono multipli solo di 5,
+    attraverso l'aggiunta di un elemento Html stampa Buzz*/
+    } else if (i % 5 === 0) {  
+        containerHtml.innerHTML += `<div class="box">Buzz</div>`;
+    //altrimenti stampa i numeri in successione
+    } else {
+        containerHtml.innerHTML += `<div class="box">${i}</div>`; 
+    }
+}
 
 
 
