b83ef30ebf3e9591dd04328a533e7543567b16af
bealabea <beatricemarin89@yahoo.it>
2021-12-10 14:36:43 +0100

    MILESTONE 1 procedure

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// creo un ciclo for per stampare in console log i numeri dall'1 al 100
+
+//MILESTONE 1:
+//Se i miei numeri sono multipli sia di 3 che di 5 stampa in console log FizzBuzz
+//altrimenti se sono multipli solo di 3 stampa in console log Fizz
+//altrimenti se sono multipli solo di 5 stampa in console log Buzz
+//altrimenti stampa in console log i numeri in successione
